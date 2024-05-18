460ccacde633e1cb4aac753d234477ea6c78de15
Alex Jarabi <jarabialex@gmail.com>
2024-05-18 15:08:28 +0300

    Refactor functions into once reusable function

--- index.js
+++ index.js
@@ -7,52 +7,28 @@ const massOutput = document.querySelector('.mass');
 
 // Event listeners
 
-convertBtn.addEventListener('click', runConversions);
+convertBtn.addEventListener('click', () => {
+  const unit = Number(input.value);
+  const length = convert(unit, new Decimal(3.281));
+  const volume = convert(unit, new Decimal(0.264));
+  const mass = convert(unit, new Decimal(2.204));
+
+  lengthOutput.textContent = `${unit} meters = ${length.imperial} feet | ${unit} feet = ${length.metric} meters`;
+  volumeOutput.textContent = `${unit} liters = ${volume.imperial} gallons | ${unit} gallons = ${volume.metric} liters`;
+  massOutput.textContent = `${unit} kilos = ${mass.imperial} pounds | ${unit} pounds = ${mass.metric} kilos`;
+});
 
 // Functions
 
 /**
- * This function runs all the conversion functions
- * when the convert button is clicked
- * @returns {void}
- */
-function runConversions() {
-  const unit = input.value;
-  const { meters, feet } = convertLength(unit);
-  const { liters, gallons } = convertVolume(unit);
-  const { kilos, pounds } = convertMass(unit);
-
-  lengthOutput.textContent = `${unit} meters = ${feet} feet | ${unit} feet = ${meters} meters`;
-  volumeOutput.textContent = `${unit} liters = ${gallons} gallons | ${unit} gallons = ${liters} liters`;
-  massOutput.textContent = `${unit} kilos = ${pounds} pounds | ${unit} pounds = ${kilos} kilos`;
-}
-
-/**
  * @param {number} unit
+ * @param {number} ratio
  * @returns {object}
  */
-function convertLength(unit) {
-  const meters = Number(unit / 3.281).toFixed(3);
-  const feet = Number(unit * 3.281).toFixed(3);
+function convert(unit, ratio) {
+  const decimalUnit = new Decimal(unit);
+  const metric = decimalUnit.dividedBy(ratio).toFixed(3);
+  const imperial = decimalUnit.times(ratio).toFixed(3);
 
-  return { meters, feet };
-}
-
-/**
- *
- * @param {number} unit
- * @returns {object}
- */
-function convertVolume(unit) {
-  const liters = Number(unit / 0.264).toFixed(3);
-  const gallons = Number(unit * 0.264).toFixed(3);
-
-  return { liters, gallons };
-}
-
-function convertMass(unit) {
-  const kilos = Number(unit / 2.204).toFixed(3);
-  const pounds = Number(unit * 2.204).toFixed(3);
-
-  return { kilos, pounds };
+  return { metric, imperial };
 }
